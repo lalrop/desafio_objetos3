05f0b48b3e1eec33e3e05f4cea349e6b42a5648f
luis rodriguez <lalrop92@gmail.com>
2018-02-01 23:31:08 -0300

    ejercicio 5 listo, falta terminar ejercicio 4

--- desafio.rb
+++ desafio.rb
@@ -108,3 +108,57 @@ end
 
 Alumno.read_file('notas.txt')
 x = Alumno.new('Gaspar',7,4,3,2)
+
+# # Ejercicio 5
+# class Rectangulo
+#
+#   def initialize(largo, ancho)
+#   @largo = largo.to_f
+#   @ancho = ancho.to_f
+#   end
+#
+#   def lados
+#     puts "Este Rectangulo mide #{@largo}cm de largo y #{@ancho}cm de ancho"
+#   end
+#
+#   def perimetro
+#     resultado = (2*@largo) + (2*@ancho)
+#     puts "El perimetro del Rectangulo es #{resultado}"
+#   end
+#
+#   def area
+#     resultado = @largo*@ancho
+#     puts "El area del Rectangulo es #{resultado}"
+#   end
+#
+# end
+#
+# class Cuadrado
+#
+#   def initialize(lado)
+#   @lado = lado
+#   end
+#
+#   def lados
+#   puts "Este Cuadrado mide #{@largo}cm de largo y #{@ancho}cm de ancho"
+#   end
+#
+#   def perimetro
+#     resultado = 4*@lado
+#     puts "El perimetro del Cuadrado es #{resultado}"
+#   end
+#
+#   def area
+#     resultado = @lado**2
+#     puts "El area del Cuadrado es #{resultado}"
+#   end
+#
+# end
+#
+# rectangulo1 = Rectangulo.new(2,4)
+# cuadrado1 = Cuadrado.new(2)
+#
+# rectangulo1.perimetro
+# rectangulo1.area
+# cuadrado1.perimetro
+# cuadrado1.area
